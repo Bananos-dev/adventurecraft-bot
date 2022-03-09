ebfd63e512dc49acfa6c41ae4fbb637afd443ddc
DevMax <devmaximum@yahoo.com>
2022-03-10 00:22:02 +0530

    add: history command

---
+++ Commands/moderation/history.cmd.js
@@ -0,0 +1,88 @@
+const { Constants, CommandInteraction } = require("discord.js");
+const config = require("../../config.json");
+const { postLog } = require("../../Service/logs.service");
+const {
+	getErrorReplyContent,
+	getSuccessReplyContent,
+} = require("../../Utils/common.util");
+const memberPunishmentSchema = require("../../Schemas/member-punishment-schema");
+
+module.exports = {
+	name: "history",
+	description: "View punishment history of a member",
+	options: [
+		{
+			name: "member",
+			description: "member in the server",
+			type: Constants.ApplicationCommandOptionTypes.USER,
+			required: true,
+		},
+	],
+	/**
+	 *
+	 * @param {CommandInteraction} interaction
+	 */
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+
+		const executedMember = await interaction.guild.members.fetch(
+			interaction.user
+		);
+
+		if (
+			!executedMember.roles.cache.get(config.mod_role_id) &&
+			!executedMember.permissions.has("ADMINISTRATOR")
+		) {
+			return interaction.editReply(
+				getErrorReplyContent("You don't have permission to run this command.")
+			);
+		}
+
+		const user = interaction.options.getUser("member", true);
+
+		const history = await memberPunishmentSchema
+			.find({
+				targetUserId: user.id,
+			})
+			.sort({ createdAt: -1 })
+			.limit(5);
+
+		if (history.length === 0) {
+			return interaction.editReply(
+				getSuccessReplyContent(
+					"This member doesn't have any punishment history."
+				)
+			);
+		}
+
+		await interaction.editReply(
+			getSuccessReplyContent("Member punishment history has been retrieved.")
+		);
+
+		for (const h of history) {
+			await interaction.channel.send({
+				embeds: [
+					{
+						color: "BLURPLE",
+						fields: [
+							{ name: "ID", value: h.id.toString() },
+							{ name: "Action", value: h.action },
+							{
+								name: "Target User",
+								value: `${h.targetUserTag}(<@${h.targetUserId}>)`,
+							},
+							{
+								name: "Executed User",
+								value: `${h.executedUserTag}(<@${h.executedUserId}>)`,
+							},
+							{
+								name: "Date",
+								value: `<t:${Math.floor(h.createdAt.getTime() / 1000)}:F>`,
+							},
+						],
+					},
+				],
+			});
+		}
+	},
+};
--- Schemas/member-punishment-schema.js
+++ Schemas/member-punishment-schema.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose");
 
+const counterSchema = new mongoose.Schema({
+	_id: {
+		type: String,
+		required: true,
+	},
+	seq: {
+		type: Number,
+		default: 0,
+	},
+});
+
+counterSchema.static("increment", async function (counterName) {
+	const count = await this.findByIdAndUpdate(
+		counterName,
+		{ $inc: { seq: 1 } },
+		{ new: true, upsert: true }
+	);
+	return count.seq;
+});
+
+const CounterModel = mongoose.model("Counter", counterSchema);
+
 const memberPunishmentSchema = new mongoose.Schema(
 	{
 		id: { type: Number, default: 0 },
@@ -29,17 +51,10 @@ const memberPunishmentSchema = new mongoose.Schema(
 	{ timestamps: true }
 );
 
-memberPunishmentSchema.pre("save", function (next) {
-	var doc = this;
-	counter.findByIdAndUpdate(
-		{ _id: "entityId" },
-		{ $inc: { id: 1 } },
-		function (error, counter) {
-			if (error) return next(error);
-			doc.testvalue = counter.seq;
-			next();
-		}
-	);
+memberPunishmentSchema.pre("save", async function () {
+	if (!this.isNew) return;
+	const inc = await CounterModel.increment("entity");
+	this.id = inc;
 });
 
 module.exports = mongoose.model("memberPunishment", memberPunishmentSchema);
