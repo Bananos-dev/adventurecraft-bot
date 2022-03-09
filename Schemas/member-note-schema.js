080beba990b163fc894d6ce81aceda063ea4cc0a
DevMax <devmaximum@yahoo.com>
2022-03-09 23:54:17 +0530

    add: note command

--- Schemas/member-note-schema.js
+++ Schemas/member-note-schema.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const memberNoteSchema = new mongoose.Schema(
 	{
 		executedUserId: String,
-		userId: String,
+		executedUserTag: String,
+		targetUserId: String,
+		targetUserTag: String,
 		note: String,
 	},
 	{ timestamps: true }
--- Schemas/member-punishment-schema.js
+++ Schemas/member-punishment-schema.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const memberPunishmentSchema = new mongoose.Schema(
 	{
+		id: { type: Number, default: 0 },
 		action: {
 			type: String,
 			enum: ["ban", "timeout", "kick", "warn"],
@@ -27,5 +28,18 @@ const memberPunishmentSchema = new mongoose.Schema(
 	},
 	{ timestamps: true }
 );
+
+memberPunishmentSchema.pre("save", function (next) {
+	var doc = this;
+	counter.findByIdAndUpdate(
+		{ _id: "entityId" },
+		{ $inc: { id: 1 } },
+		function (error, counter) {
+			if (error) return next(error);
+			doc.testvalue = counter.seq;
+			next();
+		}
+	);
+});
 
 module.exports = mongoose.model("memberPunishment", memberPunishmentSchema);
